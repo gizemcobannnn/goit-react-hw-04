ee1cafe4ba6aa72642330af9a213369fb4b3e90a
gizemcoban <gizem.coban.r@gmail.com>
2024-11-20 16:27:26 +0300

    axios library loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import SearchBox from './components/SearchBox.jsx'
 import './App.css'
+import axios from 'axios';
 
 function App() {
  
+  const baseUrl="https://api.unsplash.com/";
+  const path="search/photos";
 
   return (
     <>
